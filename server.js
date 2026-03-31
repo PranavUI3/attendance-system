7f7e0c807580f9f3218978ab069186f6813c11ad
Pranav Sharma <sharmapranav98766@gmail.com>
2026-03-31 04:25:16 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/",(req,res)=>{
+  res.send("server is running");
+});
+
 // const db = mysql.createConnection({
 //   host: "3000",
 //   user: "root",
