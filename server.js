3a281d0eea91b1754628a1847f25d5c0b04b06c7
Pranav Sharma <sharmapranav98766@gmail.com>
2026-04-02 23:54:57 -0700

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 // Railway gives PORT automatically
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // MySQL connection (Railway env).
 const db = mysql.creatConnection({
