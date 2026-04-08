77767f75db93b1329f176f1e772d6e8e3d65aece
Pranav Sharma <sharmapranav98766@gmail.com>
2026-04-08 05:00:28 -0700

    Update Student_login.js

--- Student_login.js
+++ Student_login.js
@@ -84,7 +84,7 @@ submitbtn.addEventListener("click", async (e) => {
   submitbtn.disabled = true;
 
   try {
-    const response = await fetch("http://localhoast:3000/api", {
+    const response = await fetch("http://localhost:3000/add", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
