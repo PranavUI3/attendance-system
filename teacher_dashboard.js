957b9e0d5ae432b79530432570d8efce71872a73
Pranav Sharma <sharmapranav98766@gmail.com>
2026-04-08 04:42:07 -0700

    Update teacher_dashboard.js

--- teacher_dashboard.js
+++ teacher_dashboard.js
@@ -5,7 +5,8 @@ let students = [];
 
 async function loadAttendance() {
   try {
-    const response = await fetch("http://localhost:3000/attendance");
+    // const response = await fetch("http://localhost:3000/attendance");
+    const response = await fetch("attendance-system-production-1bc7.up.railway.app");
     const data = await response.json();
 
     students = data;
